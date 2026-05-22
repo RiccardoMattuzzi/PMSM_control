7514c999f09dcee09e923f8388b41283ff0635ec
Riccardo <riccardomattuzzi@gmail.com>
2026-05-22 09:00:50 +0000

    MATLAB/Simulink files added.

---
+++ data.m
@@ -0,0 +1,29 @@
+%% Always add
+
+close all; clear all; clc;
+addpath(fullfile(matlabroot,'toolbox/physmod/sm/sli/m'));
+
+open("model.slx");
+
+
+
+%% Data
+
+Rs = 1;
+Ls = 1;
+p = 1;
+Amg = 1;
+J = 1;
+B = 1;
+
+
+
+
+
+
+
+
+
+
+
+% END
